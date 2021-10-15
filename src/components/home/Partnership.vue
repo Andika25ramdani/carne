d54ed17972297cfc6ab700b816122d1ac77f1b47
Andika .R <andika15ramdani@gmail.com>
2021-10-16 02:57:49 +0700

    20211016 - 0257 - partnership

---
+++ src/components/home/Partnership.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="partnership text-green pb-16">
+		<h2 class="font-black text-center title-text pt-8 pb-16">Who Have Worked With Us Before?</h2>
+		<div class="grid grid-cols-1 sm:grid-cols-2 gap-y-8 sm:gap-y-16 items-center w-2/3 sm:w-5/6 mx-auto">
+      <a href="http://" target="_blank" rel="noopener noreferrer">
+        <img src="/img/partners/roots.png" alt="Roots">
+      </a>
+      <a href="http://" target="_blank" rel="noopener noreferrer">
+        <img src="/img/partners/jimmyandmartin.png" alt="Jimmy And Martin">
+      </a>
+      <a href="http://" target="_blank" rel="noopener noreferrer">
+        <img src="/img/partners/memento.png" alt="Memento Coffee">
+      </a>
+      <a href="http://" target="_blank" rel="noopener noreferrer">
+        <img src="/img/partners/pizzaplace.png" alt="Pizza Place">
+      </a>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+  name: 'Partnership'
+}
+</script>
+
+<style lang="sass">
+.partnership img
+  max-width: 345px
+  height: 100%
+  margin: 0 auto
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
     <FindUs />
     <CantVisit />
     <Available />
+    <Partnership />
     <BestMenu />
     <Contact />
   </div>
@@ -20,11 +21,12 @@ import BestMenu from '@/components/home/BestMenu.vue'
 import CantVisit from '@/components/home/CantVisit.vue'
 import Contact from '@/components/home/Contact.vue'
 import FindUs from '@/components/home/FindUs.vue'
+import Partnership from '@/components/home/Partnership.vue'
 
 export default {
   name: 'Home',
   components: {
-    Available, Banner, Begining, BestMenu, CantVisit, Contact, FindUs
+    Available, Banner, Begining, BestMenu, CantVisit, Contact, FindUs, Partnership
   }
 }
 </script>
